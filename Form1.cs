15c45e471ce1a99980c319412768fe0d03fdb47b
Tran-Nguyen-Anh-Trinh <80233430+Tran-Nguyen-Anh-Trinh@users.noreply.github.com>
2021-04-29 20:39:39 +0700

    UI_Login

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logindn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetData();
+        }
+        public void SetData()
+        {
+            lg1.Phone = "Phone";
+            lg1.Password = "Password";
+        }
+    }
+}
---
+++ LG.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logindn
+{
+    public partial class LG : UserControl
+    {
+        public LG()
+        {
+            InitializeComponent();
+        }
+        public string Phone
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                textBox1.Text = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return textBox2.Text;
+            }
+            set
+            {
+                textBox2.Text = value;
+            }
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Phone")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Phone";
+                textBox1.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Password")
+            {
+                textBox2.PasswordChar = '*';
+                textBox2.Text = "";
+                textBox2.ForeColor = SystemColors.WindowText;
+            }
+        }
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.PasswordChar = '\0';
+                textBox2.Text = "Password";
+                textBox2.ForeColor = SystemColors.GrayText;
+            }
+        }
+        private void button_WOC1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đăng nhập được rồi nè thằng lồn");
+        }
+    }
+}
